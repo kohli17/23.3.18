f67c6a9b404a3dea7c77ca7e2d51f9fe556966c6
kohli17 <35589866+kohli17@users.noreply.github.com>
2018-03-23 15:48:46 +0530

    Create small number.c
    print the number

---
+++ small number.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+#include<conio.h>
+void main()
+{
+    int x,y,i,s;
+    printf("Enter the two numbers:");
+    scanf("%d%d",&x,&y);
+    for(i=0;i<=x||i<=y;i++)
+    {
+        if(i%x==0&&i%y==0)
+        {
+            s=i;
+        }
+    }
+    printf("\nThe smallest number divide by both x&y are:%d",s);
+    
+}
